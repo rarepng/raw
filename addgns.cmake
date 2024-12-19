cbf704b6df2dedb4cd77a777bc37d615a0572ff8
clover <inksomnia6661@gmail.com>
2024-12-19 14:40:57 +0200

    cmake includes

---
+++ addgns.cmake
@@ -0,0 +1,1 @@
+add_subdirectory(./sub/gns ./cmake_more_cache/gns)
---
+++ instproto.cmake
@@ -0,0 +1,3 @@
+
+include("E:/silver/qtcan/raw/cmake_more_cache/absl/cmake_install.cmake")
+include("E:/silver/qtcan/raw/cmake_more_cache/proto/cmake_install.cmake")
